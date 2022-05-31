704145c4bc388ad40cfae57a524ca08add63358b
Alicia <9159818+awongstory@users.noreply.github.com>
2022-05-31 14:51:23 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,52 +1,69 @@
-while 1:
-    try:
-        first_num = input("This will make up your fizz. What number would you like to replace?")
-        first_num = int(first_num)
-    except ValueError:
-        print("User string is not a number.")
-        continue
-    print("Your first number is", first_num)
-    break
-    
-while 1:
-    try: 
-        second_num = input("This will make up your buzz. What number would you like to replace?")
-        second_num = int(second_num)
-    except ValueError:
-        print("User string is not a number.")
-        continue
-    print("Your second number is", second_num)
-    break
-    
-while 1:
-    try:
-        user_min_range = input("What is the minimum number in your range?")
-        user_min_range = int(user_min_range)
-    except ValueError:
-        print("User string is not a number.")
-        continue
-    print("Your minimum number is", user_min_range)
-    break
-    
-while 1: 
-    try:
-        user_max_range = input("What is the maximum number in your range?")
-        user_max_range = int(user_max_range)
-    except ValueError:
-        print("User string is not a number.")
-        continue
-    print("Your maximum number is", user_max_range)
-    break
-    
-for i in range(user_min_range, user_max_range):
-    if i % first_num == 0 and i % second_num == 0:
-        print("fizzbuzz")
-    elif i % first_num == 0:
-        print("fizz")
-    elif i % second_num == 0:
-        print("buzz")
-    else:
-        print(i)
-print("Would you like to try again?")
+def fizzbuzz():
 
+    while 1:
+        try:
+            first_num = input("This will make up your fizz. What number would you like to replace?")
+            first_num = int(first_num)
+        except ValueError:
+            print("User string is not a number.")
+            continue
+        print("Your first number is", first_num)
+        break
 
+    while 1:
+        try:
+            second_num = input("This will make up your buzz. What number would you like to replace?")
+            second_num = int(second_num)
+        except ValueError:
+            print("User string is not a number.")
+            continue
+        print("Your second number is", second_num)
+        break
+
+    while 1:
+        try:
+            user_min_range = input("What is the minimum number in your range?")
+            user_min_range = int(user_min_range)
+        except ValueError:
+            print("User string is not a number.")
+            continue
+        print("Your minimum number is", user_min_range)
+        break
+
+    while 1:
+        try:
+            user_max_range = input("What is the maximum number in your range?")
+            user_max_range = int(user_max_range)
+        except ValueError:
+            print("User string is not a number.")
+            continue
+        print("Your maximum number is", user_max_range)
+        break
+
+    for i in range(user_min_range, user_max_range):
+        if i % first_num == 0 and i % second_num == 0:
+            print("fizzbuzz")
+        elif i % first_num == 0:
+            print("fizz")
+        elif i % second_num == 0:
+            print("buzz")
+        else:
+            print(i)
+
+    while 1:
+        try:
+            answer = input("Would you like to try again? 1 = yes, 0 = no")
+            answer = int(answer)
+        except ValueError:
+            print("User string is not a number.")
+            continue
+        if answer != 1 and answer != 0:
+            print("That's not a valid response. Please select between 0 and 1.")
+        elif answer == 0:
+            print("Thanks for playing!")
+            break
+        else:
+            fizzbuzz()
+            break
+
+fizzbuzz()
